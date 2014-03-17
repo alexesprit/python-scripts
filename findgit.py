43d1ecc17e01ab1cb3785bcb441b64f3c53cf605
alexesprit <alex.esprit@gmail.com>
2014-03-17 22:53:32 +0400

    findgit: update variables names

--- findgit.py
+++ findgit.py
@@ -10,20 +10,20 @@ TYPE_LOCAL = 1 << 1
 TYPE_REMOTE = 1 << 2
 
 
-def get_repo_type(repo_path):
-    refs_path = os.path.join(repo_path, '.git', 'refs')
+def get_repo_type(repo_directory):
+    refs_path = os.path.join(repo_directory, '.git', 'refs')
     return TYPE_REMOTE if 'remotes' in os.listdir(refs_path) else TYPE_LOCAL
 
 
-def find_git_repos(folder, search_mask):
-    child_folders = os.listdir(folder)
-    if '.git' in child_folders:
-        repo_type = get_repo_type(folder)
+def find_git_repos(directory, search_mask):
+    child_dirs = os.listdir(directory)
+    if '.git' in child_dirs:
+        repo_type = get_repo_type(directory)
         if repo_type & search_mask:
-            print u'Found git repo at %s' % folder
+            print u'Found git repo at %s' % directory
     else:
-        for child_item in child_folders:
-            child_item_path = os.path.join(folder, child_item)
+        for child_item in child_dirs:
+            child_item_path = os.path.join(directory, child_item)
             if os.path.isdir(child_item_path):
                 find_git_repos(child_item_path, search_mask)
 
