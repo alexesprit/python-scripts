cb0e16205d5e8321406a1812340a6619bbab221e
alexesprit <alex.esprit@gmail.com>
2015-12-12 15:30:07 +0300

    Add `which` script
    It equals Linux `which`

---
+++ which.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+
+EXECUTABLES = ('.exe', '.bat', '.cmd')
+
+
+def is_executable(fpath):
+    return os.path.isfile(fpath) and os.access(fpath, os.X_OK)
+
+
+def which(program):
+    fpath, fname = os.path.split(program)
+    if fpath:
+        if is_exe(program):
+            return program
+    else:
+        for path in os.environ['PATH'].split(os.pathsep):
+            exe_files = []
+            extension = os.path.splitext(program)[1]
+            if not extension:
+                for extension in EXECUTABLES:
+                    exe_file = os.path.join(path, '{0}{1}'.format(program, extension))
+                    exe_files.append(exe_file)
+            else:
+                exe_files = [os.path.join(path, program)]
+
+            for exe_file in exe_files:
+                if is_executable(exe_file):
+                    return exe_file
+
+    return None
+
+
+def main(args):
+    program = ''.join(args[1:])
+    program_path = which(program)
+    if program_path:
+        print(program_path)
+    else:
+        print('{0}: not found'.format(program))
+    return 0
+
+
+if '__main__' == __name__:
+    sys.exit(main(sys.argv))
